85a7178c650b9cc7ab8a218b335271aaa00b7e56
kubraacelik <kkbra.celik92@gmail.com>
2024-03-17 21:12:45 +0300

    added weatherData from weather api

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
 
   const [weatherData, setWeatherData] = useState(null);
 
-  const handleLocationChange = () => {};
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,15 +21,17 @@ function App() {
           }&q=${location}&days=4&aqi=yes&alerts=yes`
         );
         setWeatherData(response.data);
+        console.log(response);
       } catch (error) {
         console.log(error);
       }
     };
+
     if (location) {
       fetchData();
     }
   }, [location]);
-  
+
   return (
     <>
       <div className="app-container">
@@ -41,6 +45,23 @@ function App() {
           />
         </div>
       </div>
+
+      {weatherData && (
+        <div className="weather-container">
+          {weatherData.forecast.forecastday.map((day) => (
+            <div className="day-container" key={day.date}>
+              <h2 className="date">{day.date}</h2>
+              <img
+                className="weather-icon"
+                src={day.day.condition.icon}
+                alt={day.day.condition.text}
+              />
+              <p className="temperature">{day.day.avgtemp_c} C</p>
+              <p className="temperature">{day.day.condition.text}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
