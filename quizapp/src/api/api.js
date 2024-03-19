dd07ad26dcabaeb6dbc993e3978b3aa5146473a1
kubraacelik <kkbra.celik92@gmail.com>
2024-03-19 16:09:28 +0300

    Data from the API is retrieved and assigned to the data variable

--- quizapp/src/api/api.js
+++ quizapp/src/api/api.js
@@ -1,5 +1,13 @@
+const shuffleArray = (array) => {
+  return [...array].sort(() => Math.random() - 0.5);
+};
+
 export const fetchQuizData = async (difficulty, amount) => {
   const url = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
-  const data= await (await fetch(url)).json();
-  return data.results;
+  const data = await (await fetch(url)).json();
+  return data.results.map((dt) => ({
+    ...dt,
+    //doğru cevabın hep aynı şıkta olmaması için yazdık
+    answer: shuffleArray([...dt.incorrect_answers, dt.correct_answer]),
+  }));
 };
