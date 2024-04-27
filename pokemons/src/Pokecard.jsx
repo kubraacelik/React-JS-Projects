ea36f6e5af2a3fbdc13f6eea7e00a191aa5ad127
kubraacelik <kkbra.celik92@gmail.com>
2024-04-27 17:46:20 +0300

    API URL for receiving Pokemon images given

--- pokemons/src/Pokecard.jsx
+++ pokemons/src/Pokecard.jsx
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 
 //! HER KARTA ÖZEL OLAN KISIM
 
-// Pokemon resimlerinin alınacağı API URL'si 
+// Pokemon resimlerinin alınacağı API URL'sini verdik
 const POKE_API =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
 
 class Pokecard extends Component {
     // render yöntemi, bileşenin görüntülenmesi için JSX döndürür. Bu, bileşenin arayüzünü oluşturur.
   render() {
+    // imgSrc değişkeni, Pokemon resminin URL'sini oluşturmak için kullanılır. 
+    // Bu URL, PokeAPI'ye giderek, Pokemon'un resmini çekecek şekilde ID'ye göre düzenlenir.
     let imgSrc = `${POKE_API}${this.props.id}.png`;
     return (
       <div className="Pokecard">
@@ -17,7 +19,7 @@ class Pokecard extends Component {
           <img src={imgSrc} alt={this.props.name} />
         </div>
         <div className="Pokecard-data">Type : {this.props.type}</div>
-        <div className="Pokecard-data">Exp : {this.props.base_experience}</div>
+        <div className="Pokecard-data">Exp : {this.props.exp}</div>
       </div>
     );
   }
